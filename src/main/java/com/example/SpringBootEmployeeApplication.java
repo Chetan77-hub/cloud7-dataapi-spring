ad207c1d0fe7c2f29c0ee3aac3b3d4b077de228e
Chetan77-hub <56333502+Chetan77-hub@users.noreply.github.com>
2025-05-25 14:32:42 +0530

    Update SpringBootEmployeeApplication.java

--- src/main/java/com/example/SpringBootEmployeeApplication.java
+++ src/main/java/com/example/SpringBootEmployeeApplication.java
@@ -1,12 +1,1 @@
-package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication  // <== This annotation is required here
-public class SpringBootEmployeeApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootEmployeeApplication.class, args);
-    }
-}
