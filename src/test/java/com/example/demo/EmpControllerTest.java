450d0f0f6b655de0d3d8ba49cd5adc879d7b9d8d
Chetan77-hub <56333502+Chetan77-hub@users.noreply.github.com>
2025-05-25 14:09:36 +0530

    Update EmpControllerTest.java

--- src/test/java/com/example/demo/EmpControllerTest.java
+++ src/test/java/com/example/demo/EmpControllerTest.java
@@ -10,8 +10,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(EmployeeController.class)
-public class EmployeeControllerTest {
+@WebMvcTest(EmpController.class)
+public class EmpControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
